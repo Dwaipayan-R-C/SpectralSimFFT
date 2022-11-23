08778e9b9f88fc0892d3644a8ab543e0ef23232c
Dwaipayan-R-C <dwaipayan0502@gmail.com>
2022-11-23 17:14:45 +0530

    Interpolation dft working

--- fft_interpolation.py
+++ fft_interpolation.py
@@ -1,13 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-L = 1
-N = 1000
-dx = L/N
-x = np.arange(N)*dx
-sample_points = x[::100] # Sample every 100th point
-f = lambda x: np.sin(np.pi*x)**4
-plt.plot(x, f(x), 'k-')
-plt.plot(sample_points, f(sample_points), 'rx', ms=20) 
-plt.title('A discretely sampled continuous function')
+
+# odd dimension for simplicity
+n    = 10
+npad = 300
+
+
+duration = 2*np.pi
+x    = np.arange(0., duration, duration/n)
+xpad = np.arange(0., duration, duration/npad)
+
+f = np.sin(x) 
+
+f_fwd = np.fft.fft(f)
+
+f_fwd_pad = np.zeros(npad,dtype=complex)
+
+h = (n-1)/2
+h = n - 3
+f_fwd_pad[0:h+1]   = f_fwd[0:h+1]
+f_fwd_pad[npad-h:] = f_fwd[n-h:]
+
+f_interpolated = np.fft.ifft(f_fwd_pad)*npad/n
+
+
+plt.plot(x,f,'b*')
+plt.plot(xpad,np.real(f_interpolated),color='r')
+plt.legend(['Discrete samples','Interpolated curve'])
+
+
+
 plt.show()
-print()
+
